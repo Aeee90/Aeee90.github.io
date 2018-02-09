e2dfea9e47465d69e0d5b45db2d34b1747a48e7b
Aeee90 <spoi2308@gmail.com>
2018-02-09 17:53:25 +0900

    category css update

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,17 @@ window.onload = ()=>{
     
     const cateEvent = ((base, count)=>{
         const basePoint = base, countPoint = count
-            , posts = Array.from(base.children).map( li=> {return {li, category: li.getAttribute('category')}});
+            , posts = Array.from(base.children).map( li=> {return {li, category: `#${li.getAttribute('category')}`}});
         return (e)=>{
             base.innerHTML = '';
             const cateName = e.target.textContent;
             let count = 0;
-            posts.filter(v => cateName ==='ALL'? true : v.category===cateName).forEach((v,i)=>{ count++; base.appendChild(v.li);});
+            posts.filter(v => cateName ==='#ALL'? true : v.category===cateName).forEach((v,i)=>{ count++; base.appendChild(v.li);});
             countPoint.textContent = `${count} Posts`;
         }
     })(document.querySelector('#post-list'), document.querySelector('#navbar h5'));
     
-    const category = Array.from(document.querySelector('#category .content_section').getElementsByTagName('div'));
+    const category = Array.from(document.querySelector('#category .content_section').getElementsByTagName('span'));
     category.forEach((div,i)=>{
         div.onclick = cateEvent;
     });
